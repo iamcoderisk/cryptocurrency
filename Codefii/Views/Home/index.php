1ab0dfa04e679479cd3eb77326c74c7a0b763ba9
iamcoderisk <ekeminyd@gmail.com>
2018-01-07 07:18:49 +0100

    added

--- Codefii/Views/Home/index.php
+++ Codefii/Views/Home/index.php
@@ -6,8 +6,68 @@
     <meta name="content" content="" />
     <meta name="description" content="" />
     <title>Bittrust currency | Home</title>
+    <!-- css -->
+    <script src="./public/jquery.min.js"></script>
+    <link rel="stylesheet" href="./public/css/app.css" />
+    <link rel="stylesheet" href="./public/css/bootstrap.css" />
+
   </head>
   <body>
-    
+    <header id="particles-js">
+      <br />
+      <div class="mid-header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-faded">
+                   <a class="navbar-brand" href="#">Bittrust</a>
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                   </button>
+                   <div id="navbarNavDropdown" class="navbar-collapse collapse">
+                       <ul class="navbar-nav mr-auto">
+
+
+                       </ul>
+                       <ul class="navbar-nav">
+                           <li class="nav-item">
+                               <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                 HOME
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="{{ url('/login') }}">ROADMAP</a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="{{ url('/register') }}">TRUST EXCHANGE</a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="{{ url('/register') }}">ICO CROWD SALES</a>
+                           </li>
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">WHITE PAPER</a>
+                           </li>
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">FAQ</a>
+                           </li>
+                       </ul>
+                   </div>
+               </nav>
+        </div>
+      </div>
+    </header>
+<div class="bg-inkfo">
+
+</div>
+
+    <!-- scripts -->
+    <script src="./public/js/jquery.min.js"></script>
+        <script src="./public/js/popper.min.js"></script>
+    <script src="./public/js/bootstrap.js"></script>
+
+      <!-- <script src="./public/js/particles.js"></script>
+    <script src="./public/js/app.js"></script> -->
+
+
+
+
   </body>
 </html>
